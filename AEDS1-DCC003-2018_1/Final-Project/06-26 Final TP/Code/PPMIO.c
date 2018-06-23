bde398c742c8f59f34fdf85bfcce91dd9eee6bdf
Lucas Pellegrinelli <lucaspellegrinelli@hotmai.com>
2018-06-23 01:17:18 -0300

    Fixed Identation size

--- AEDS1-DCC003-2018_1/Final-Project/06-26 Final TP/Code/PPMIO.c
+++ AEDS1-DCC003-2018_1/Final-Project/06-26 Final TP/Code/PPMIO.c
@@ -12,82 +12,82 @@
 #define RGB_NUMBER_COMPONENTS 3
 
 struct Image read_image(char *path){
-	struct Image output;
+  struct Image output;
 
-	const char* open_mode = "r";
-	FILE *image_file = fopen(path, open_mode);
+  const char* open_mode = "r";
+  FILE *image_file = fopen(path, open_mode);
 
-	int header_count = 0;
-	int body_count = 0;
-	int rgb_component_index = 0;
-	int comment_lines = 0;
+  int header_count = 0;
+  int body_count = 0;
+  int rgb_component_index = 0;
+  int comment_lines = 0;
 
-	char **header = (char **)malloc(sizeof(char *) * HEADER_MAX_LINES);
+  char **header = (char **)malloc(sizeof(char *) * HEADER_MAX_LINES);
 
-	char *buffer = NULL;
-	size_t len = 0;
-	ssize_t read;
+  char *buffer = NULL;
+  size_t len = 0;
+  ssize_t read;
 
-	// Lê os headers do arquivo (tipo, tamanho, comentários...)
-	while ((read = getline(&buffer, &len, image_file)) != -1) {
-		if(header_count < comment_lines + STANDARD_HEADER_LINES){
-			header[header_count] = malloc(sizeof(char) * HEADER_LINE_LENGTH);
-			if(buffer[0] == '#') comment_lines++; // Suporte para comentários
-			strcpy(header[header_count++], buffer); // Copia a linha atual para a header
-		}else break;
-	}
+  // Lê os headers do arquivo (tipo, tamanho, comentários...)
+  while ((read = getline(&buffer, &len, image_file)) != -1) {
+    if(header_count < comment_lines + STANDARD_HEADER_LINES){
+      header[header_count] = malloc(sizeof(char) * HEADER_LINE_LENGTH);
+      if(buffer[0] == '#') comment_lines++; // Suporte para comentários
+      strcpy(header[header_count++], buffer); // Copia a linha atual para a header
+    }else break;
+  }
 
-	char *splitted_dimensions = strtok(header[STANDARD_DIMENSION_INDEX + comment_lines], " "); // Divide o texto com as dimensões
-	output.width = atoi(splitted_dimensions); // Coloca a largura
-	splitted_dimensions = strtok(NULL, " \t"); // Pula para a altura
-	output.height = atoi(splitted_dimensions); // Coloca a altura
+  char *splitted_dimensions = strtok(header[STANDARD_DIMENSION_INDEX + comment_lines], " "); // Divide o texto com as dimensões
+  output.width = atoi(splitted_dimensions); // Coloca a largura
+  splitted_dimensions = strtok(NULL, " \t"); // Pula para a altura
+  output.height = atoi(splitted_dimensions); // Coloca a altura
 
-	int **body = (int **)malloc(sizeof(int *) * output.width * output.height);
-	body[0] = malloc(sizeof(int) * RGB_NUMBER_COMPONENTS);
+  int **body = (int **)malloc(sizeof(int *) * output.width * output.height);
+  body[0] = malloc(sizeof(int) * RGB_NUMBER_COMPONENTS);
 
-	do{ // Do .. While pois temos a primeira linha da imagem como resto no buffer de quando pegamos a header
-		int buffer_size = 0;
-		for(int i = 0; i < strlen(buffer); i++){
-			if(buffer[i] == ' ' || buffer[i] == '\t' || buffer[i] == '\r' || buffer[i] == '\n' || buffer[i] == '\v' || buffer[i] == '\f')
-				buffer_size++;
-		}
+  do{ // Do .. While pois temos a primeira linha da imagem como resto no buffer de quando pegamos a header
+    int buffer_size = 0;
+    for(int i = 0; i < strlen(buffer); i++){
+      if(buffer[i] == ' ' || buffer[i] == '\t' || buffer[i] == '\r' || buffer[i] == '\n' || buffer[i] == '\v' || buffer[i] == '\f')
+        buffer_size++;
+    }
 
-		char delimit[6] = " \t\r\n\v\f";
+    char delimit[6] = " \t\r\n\v\f";
 
-		char *rgb_components = strtok(buffer, delimit);
+    char *rgb_components = strtok(buffer, delimit);
 
-		while(rgb_components != NULL){
-			body[body_count][rgb_component_index] = atoi(rgb_components);
+    while(rgb_components != NULL){
+      body[body_count][rgb_component_index] = atoi(rgb_components);
 
-			rgb_component_index++;
-			if(rgb_component_index > 2){
-				rgb_component_index = 0;
-				body_count++;
-				body[body_count] = malloc(sizeof(int) * RGB_NUMBER_COMPONENTS);
-			}
+      rgb_component_index++;
+      if(rgb_component_index > 2){
+        rgb_component_index = 0;
+        body_count++;
+        body[body_count] = malloc(sizeof(int) * RGB_NUMBER_COMPONENTS);
+      }
 
-			rgb_components = strtok(NULL, delimit);
-		}
-	}while ((read = getline(&buffer, &len, image_file)) != -1);
+      rgb_components = strtok(NULL, delimit);
+    }
+  }while ((read = getline(&buffer, &len, image_file)) != -1);
 
-	output.header = header;
-	output.pixels = body;
+  output.header = header;
+  output.pixels = body;
 
-	return output;
+  return output;
 }
 
 void write_image(struct Image image, char *output_name){
-	FILE *fp = fopen(output_name, "wb");
+  FILE *fp = fopen(output_name, "wb");
   fprintf(fp, "P3\n%d %d\n255\n", image.height, image.width);
 
   for(int i = 0; i < image.height; i++){
     for(int j = 0; j < image.width; j++){
       fprintf(fp, "%d\n%d\n%d\n", image.pixels[j + i * image.height][0],
-														image.pixels[j + i * image.height][1],
-														image.pixels[j + i * image.height][2]);
+                            image.pixels[j + i * image.height][1],
+                            image.pixels[j + i * image.height][2]);
     }
 
-		fprintf(fp, "\n");
+    fprintf(fp, "\n");
   }
 
   fclose(fp);
--- AEDS1-DCC003-2018_1/Final-Project/06-26 Final TP/Code/PPMIO.h
+++ AEDS1-DCC003-2018_1/Final-Project/06-26 Final TP/Code/PPMIO.h
@@ -4,29 +4,29 @@
 */
 
 /*
-	Nesse código teremos a definição de todos os métodos/funções responsáveis
-	pela leitura e escrita de arquivos tipo PPM (ASCII, P3)
+  Nesse código teremos a definição de todos os métodos/funções responsáveis
+  pela leitura e escrita de arquivos tipo PPM (ASCII, P3)
 */
 
 /*
   Struct com função de armazenar as informações de uma imagem PPM
 */
 struct Image{
-	int width;
-	int height;
-	char **header;
-	int **pixels; // Linha: Cada pixel.    Coluna: Cada valor RGB
+  int width;
+  int height;
+  char **header;
+  int **pixels; // Linha: Cada pixel.    Coluna: Cada valor RGB
 };
 
 /*
   Lê a imagem PPM (ASCII, P3) informada e a armazena em um struct Image
 
-	Essa leitura funciona em todos os casos de PPM (ASCII, P3) que encontrei:
-		- Cada linha possuí 3 componentes RGB (sendo assim 1 linha por pixel)
-		- Cada linha possuí 1 componente RGB (sendo assim 3 linhas por pixel)
-		- Cada linha possuí 3 * LARGURA componentes da imagem (sendo assim uma
-		"representação" da imagem já que a distribuição dos pixels no arquivo segue
-		a disposição dos mesmos na imagem)
+  Essa leitura funciona em todos os casos de PPM (ASCII, P3) que encontrei:
+    - Cada linha possuí 3 componentes RGB (sendo assim 1 linha por pixel)
+    - Cada linha possuí 1 componente RGB (sendo assim 3 linhas por pixel)
+    - Cada linha possuí 3 * LARGURA componentes da imagem (sendo assim uma
+    "representação" da imagem já que a distribuição dos pixels no arquivo segue
+    a disposição dos mesmos na imagem)
 
   Entrada:
   'path' -> Caminho para o arquivo da imagem
@@ -38,10 +38,10 @@ struct Image read_image(char *path);
 
 /*
   Salva uma imagem tipo struct Image como um arquivo de imagem PPM (ASCII, P3)
-	no formato que o GIMP utiliza (cada componente RGB em uma linha)
+  no formato que o GIMP utiliza (cada componente RGB em uma linha)
 
   Entrada:
    image -> stuct Image com as informações da imagem a ser salva
-	 output_name -> Nome que a imagem terá ao ser salva
+   output_name -> Nome que a imagem terá ao ser salva
 */
 void write_image(struct Image image, char *output_name);
