f01401c5f9f3ef7870ebaac63c6d518aba8090cd
Lucas Pellegrinelli <lucaspellegrinelli@hotmai.com>
2018-06-23 11:52:54 -0300

    Changed read stop condition from steganography

--- AEDS1-DCC003-2018_1/Final-Project/06-26 Final TP/Code/steganography.c
+++ AEDS1-DCC003-2018_1/Final-Project/06-26 Final TP/Code/steganography.c
@@ -34,12 +34,28 @@ struct Image hide_message_in_image(struct Image original_image, long *encoded_me
   }
 
   /*
-   * Linha só com 0's para indicar a parada.
+   Linha só com 1's para indicar a parada.
+
+   Porque dá certo?
+
+   O motivo pelo qual isso poderia dar errado seria caso um bloco que não fosse
+   o último fosse todo cheio de 1s, fazendo o decodificador parar de ler antes
+   de ter lido todos os blocos. Porém para isso acontecer, como cada bloco é
+   limitado ao valor de N (bloco é mod N), então para que a representação binária
+   de um bloco fosse composta somente por 1s, então N teria que ser 2^x para qualquer
+   x, assim os blocos podem ter valor ((2^x) - 1) e assim ter só 1s na representação
+   binária. Mas visto que 2^x é uma potência de 2, só é possível obter valores desse tipo
+   pela multiplicação de outras potências de 2, mas dito que N é um produto entre
+   dois números primos e o único primo que é potência de 2 é o próprio 2, é impossível
+   obter um número no formato 2^x para N (já que só temos um primo que é potência de 2
+   e precisaríamos de pelo menos dois deles para formar 2^x), sendo assim impossível de
+   acontecer um bloco com representação binária toda igual a 1.
+
   */
   for(int i = 0; i < max_block_size_binary; i++){
     long current_rgb_comp_as_binary = as_other_base(original_image.pixels[pixel_counter][rgb_component_counter], 10, 2);
-    if(current_rgb_comp_as_binary % 10 == 1)
-      original_image.pixels[pixel_counter][rgb_component_counter] -= 1;
+    if(current_rgb_comp_as_binary % 10 == 0)
+      original_image.pixels[pixel_counter][rgb_component_counter] += 1;
 
     rgb_component_counter++;
     if(rgb_component_counter > 2){
@@ -74,7 +90,7 @@ long * recover_message_from_image(struct Image image, char delimiter, int *block
           current_block_int = 0;
           int block_value = binary_string_to_int(current_block_buffer);
 
-          if(block_value == 0) return message_blocks; // Linha de parada
+          if(block_value == pow(2, max_block_size_binary) - 1) return message_blocks; // Linha de parada, tudo 1
 
           message_blocks[message_blocks_index++] = block_value;
           *block_count = *block_count + 1;
