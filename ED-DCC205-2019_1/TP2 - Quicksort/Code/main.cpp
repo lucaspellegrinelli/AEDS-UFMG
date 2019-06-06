c75f40ff2afae4857764785b2bc7e2a0fc2034f7
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2019-06-06 09:31:58 -0300

    Updated for lambda

--- ED-DCC205-2019_1/TP2 - Quicksort/Code/main.cpp
+++ ED-DCC205-2019_1/TP2 - Quicksort/Code/main.cpp
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
 
 #include "sort_request.hpp"
-
+int a(int b){
+  return 1;
+}
 int main(int argc, char *argv[]){
   srand (time(NULL));
   SortRequest sr = SortRequest(argv[1], argv[2], argv[3], argc > 4);
   sr.run();
+
   return 0;
 }
--- ED-DCC205-2019_1/TP2 - Quicksort/Code/quick_sort.hpp
+++ ED-DCC205-2019_1/TP2 - Quicksort/Code/quick_sort.hpp
@@ -3,8 +3,8 @@
 
 #include "sort.hpp"
 #include "insertion_sort.hpp"
-#include "partition_function.hpp"
-#include "change_sort_function.hpp"
+// #include "partition_function.hpp"
+// #include "change_sort_function.hpp"
 
 #include <iostream>
 #include <string>
@@ -16,36 +16,46 @@ public:
 	}
 
 	void sort(long long *arr, int left, int right) override {
-		if(variation == "QNR"){
-			PartitionFunction *partition = new Classic();
-			ChangeSortFunction *change_sort = new NoChangeFunction();
-			iterative_sort(arr, left, right, partition, change_sort);
-		}else{
-			PartitionFunction *partition;
-			ChangeSortFunction *change_sort;
-			Sort *insertion = new InsertionSort();
-			if(this->variation == "QC"){
-				partition = new Classic();
-				change_sort = new NoChangeFunction();
-			}else if(variation == "QM3"){
-				partition = new Median();
-				change_sort = new NoChangeFunction();
-			}else if(variation == "QPE"){
-				partition = new FirstElement();
-				change_sort = new NoChangeFunction();
-			}else if(variation == "QT1"){
-				partition = new Median();
-				change_sort = new ChangeSortFunction(insertion, 0.01, right - left);
-			}else if(variation == "QT5"){
-				partition = new Median();
-				change_sort = new ChangeSortFunction(insertion, 0.05, right - left);
-			}else if(variation == "QT10"){
-				partition = new Median();
-				change_sort = new ChangeSortFunction(insertion, 0.1, right - left);
-			}
+		// if(variation == "QNR"){
+		// 	auto partition = new Classic();
+		// 	ChangeSortFunction *change_sort = new NoChangeFunction();
+		// 	iterative_sort(arr, left, right, partition, change_sort);
+		// }else{
+		// 	PartitionFunction *partition;
+		// 	ChangeSortFunction *change_sort;
+		// 	Sort *insertion = new InsertionSort();
+		// 	if(this->variation == "QC"){
+		// 		partition = new Classic();
+		// 		change_sort = new NoChangeFunction();
+		// 	}else if(variation == "QM3"){
+		// 		partition = new Median();
+		// 		change_sort = new NoChangeFunction();
+		// 	}else if(variation == "QPE"){
+		// 		partition = new FirstElement();
+		// 		change_sort = new NoChangeFunction();
+		// 	}else if(variation == "QT1"){
+		// 		partition = new Median();
+		// 		change_sort = new ChangeSortFunction(insertion, 0.01, right - left + 1);
+		// 	}else if(variation == "QT5"){
+		// 		partition = new Median();
+		// 		change_sort = new ChangeSortFunction(insertion, 0.05, right - left + 1);
+		// 	}else if(variation == "QT10"){
+		// 		partition = new Median();
+		// 		change_sort = new ChangeSortFunction(insertion, 0.1, right - left + 1);
+		// 	}
+		//
+		// 	recursive_sort(arr, left, right, partition, change_sort);
+		// }
 
-			recursive_sort(arr, left, right, partition, change_sort);
-		}
+		auto partition = [](long long *arr, int left, int right){
+			return (left + right) / 2;
+		};
+
+		auto change_sort = [](int left, int right){
+			return false;
+		};
+
+		recursive_sort(arr, left, right, partition, change_sort);
 	}
 private:
 	std::string variation;
@@ -58,8 +68,9 @@ private:
 		this->add_move(2);
 	}
 
-	int* partitionate_array(long long *arr, int left, int right, PartitionFunction*& partition){
-    long long pivot = arr[(*partition)(arr, left, right)];
+	template<typename PartitionFunction>
+	int* partitionate_array(long long *arr, int left, int right, PartitionFunction &partition){
+    long long pivot = arr[partition(arr, left, right)];
 
 		int i = left;
 		int j = right;
@@ -85,10 +96,12 @@ private:
 		return new int[2]{i, j};
 	}
 
-	void recursive_sort(long long *arr, int left, int right, PartitionFunction*& partition, ChangeSortFunction*& change_sort){
+	template<typename PartitionFunction, typename ChangeSortFunction>
+	void recursive_sort(long long *arr, int left, int right, PartitionFunction &partition, ChangeSortFunction &change_sort){
 		this->add_comparision(1); // If embaixo
-		if((*change_sort)(left, right)){
-			change_sort->get_new_sort()->sort(arr, left, right);
+		if(change_sort(left, right)){
+			InsertionSort insertion = InsertionSort();
+			insertion.sort(arr, left, right);
 		}else{
 			int *p = partitionate_array(arr, left, right, partition);
 			if(p[1] > left) recursive_sort(arr, left, p[1], partition, change_sort);
@@ -97,7 +110,8 @@ private:
 		}
 	}
 
-	void iterative_sort(long long *arr, int left, int right, PartitionFunction*& partition, ChangeSortFunction*& change_sort){
+	template<typename PartitionFunction, typename ChangeSortFunction>
+	void iterative_sort(long long *arr, int left, int right, PartitionFunction &partition, ChangeSortFunction &change_sort){
 		int stack[right - left + 1];
 		int top = -1;
 
