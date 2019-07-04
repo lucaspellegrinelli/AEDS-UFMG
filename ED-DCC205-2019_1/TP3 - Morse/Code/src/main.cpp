aa76133dd9f1cf9215d1d467ed1ccb35f6f525fd
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2019-07-04 01:46:37 -0300

    Added comments to tp3

--- ED-DCC205-2019_1/TP3 - Morse/Code/src/main.cpp
+++ ED-DCC205-2019_1/TP3 - Morse/Code/src/main.cpp
@@ -5,29 +5,37 @@
 #include "tree.hpp"
 
 int main(int argc, char *argv[]){
+  // Abre o arquivo com as definições do alfebeto morse
   std::ifstream config_file;
   config_file.open("src/morse.txt");
 
+  // Mensagem de erro caso o arquivo não consiga ser aberto
   if(!config_file){
     std::cout << "Não foi possível abrir o arquivo de configuração. Confira que o arquivo está na pasta src/ com o nome 'morse.txt'" << std::endl;
     exit(1);
   }
 
+  // Inicialização da árvore
   Tree t;
 
   std::string full_config;
   std::string key;
   std::string value;
 
+  // Leitura das linhas do arquivo de definição do morse adicionando cada
+  // item na árvore
   while(config_file >> value >> key){
     t.insert(key, value);
   }
 
   std::string line;
+  // Leitura para cada linha do input padrão
   while(std::getline(std::cin, line)){
     std::istringstream line_stream(line);
 
     std::string code;
+    // Para cada bloco em morse, traduza para alfabeto alfanumérico e escreva
+    // na saída padrão
     while(line_stream >> code){
       std::cout << (code == "/" ? " " : t.search(code));
     }
@@ -35,6 +43,8 @@ int main(int argc, char *argv[]){
     std::cout << std::endl;
   }
 
+  // Caso o argumento de escrita da árvore esteja presente, chame o método que
+  // imprime a árvore em pré ordem
   if(argc >= 2){
     t.pre_order();
   }
--- ED-DCC205-2019_1/TP3 - Morse/Code/src/tree.hpp
+++ ED-DCC205-2019_1/TP3 - Morse/Code/src/tree.hpp
@@ -4,6 +4,16 @@
 #include <string>
 #include <iostream>
 
+/*
+  Classe responsável por armazenar as informações de cada nó da árvore.
+   - std::string key: Armazena o código desse nó
+   - std::string value: Armazena qual a letra correspondente ao código desse nó
+
+   - Node *point: Ponteiro para o nó filho que representa o código desse nó
+                  seguido por um ponto.
+    - Node *line: Ponteiro para o nó filho que representa o código desse nó
+                   seguido por uma linha.
+*/
 class Node{
 public:
   std::string key;
@@ -11,6 +21,7 @@ public:
   Node *point;
   Node *line;
 
+  // Construtor vazio
   Node(){
     this->key = "";
     this->value = "";
@@ -18,11 +29,7 @@ public:
     this->line = nullptr;
   }
 
-  ~Node(){
-    delete this->point;
-    delete this->line;
-  }
-
+  // Método de escrita da árvore em pré-ordem na saída padrão.
   void pre_order(){
     if(this->key != ""){
       std::cout << this->value << " " << this->key << std::endl;
@@ -31,28 +38,56 @@ public:
     if(this->point) this->point->pre_order();
     if(this->line) this->line->pre_order();
   }
+
+  // Destrutor da classe, deleta os dois ponteiros criados
+  ~Node(){
+    delete this->point;
+    delete this->line;
+  }
 };
 
+/*
+  Classe responsável por administrar as funções básicas da árvore trie que será
+  usada para armazenar os códigos morse.
+
+  - Node *root: Nó raíz da árvore. Não armazena nenhuma informação, apenas
+                ponteiros para os nós que contém apenas um ponto ou uma linha.
+*/
 class Tree{
 public:
   Node *root;
 
+  // Construtor vazio
   Tree(){
     this->root = new Node();
   }
 
-  ~Tree(){
-    delete root;
-  }
+  /*
+    Método responsável por adicionar mais um nó na árvore, com uma chave e um
+    valor específico, caso essa chave ainda não exista na árvore.
+    Isso é feito fazendo uma pesquisa na árvore até que se encontre o nó
+    cuja chave seja igual à chave do item a ser inserido. Assim é simplesmente
+    necessário modificar o valor guardado por esse nó para o novo valor.
 
+    ENTRADA
+     - std::string key: Chave do novo valor
+     - std::string value: Novo valor
+
+    SAIDA
+     - void
+  */
   void insert(std::string key, std::string value){
     Node *curr_node = root;
 
     for(unsigned int i = 0; i < key.size(); i++){
       if(key[i] == '.'){
+        // Caso ainda não exista um caminho que adicione um ponto no código
+        // atual, crie um nó para esse caminho.
         if(!curr_node->point) curr_node->point = new Node();
         curr_node = curr_node->point;
       }else if(key[i] == '-'){
+        // Caso ainda não exista um caminho que adicione uma linha no código
+        // atual, crie um nó para esse caminho.
         if(!curr_node->line) curr_node->line = new Node();
         curr_node = curr_node->line;
       }
@@ -62,6 +97,20 @@ public:
     curr_node->value = value;
   }
 
+  /*
+    Método responsável por encontrar qual o valor correspondente à uma chave
+    específica. Isso é feito passando por cada nó, e dependendo do código,
+    escolhendo o caminho que adiciona um ponto ao código atual ou uma linha
+    ao código atual, até chegar no código desejado e assim retornando o valor
+    desse nó.
+
+    ENTRADA
+     - std::string key: Chave do valor que se deseja encontrar
+
+    SAIDA:
+     - std::string: Valor cuja chave é igual à chave recebida como parâmetro. Caso
+                    a chave não exista na árvore, uma string vazia é retornada.
+  */
   std::string search(std::string key){
     Node *curr_node = root;
 
@@ -78,8 +127,14 @@ public:
     return curr_node->value;
   }
 
+  // Método que chama a função de impressão de pré-ordem no nó raíz da árvore.
   void pre_order(){
     if(this->root) this->root->pre_order();
+  }
+
+  // Destrutor deletando o ponteiro para a raíz da árvore
+  ~Tree(){
+    delete root;
   }
 };
 
