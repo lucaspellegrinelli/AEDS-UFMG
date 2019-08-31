68f013ede22a57ad9f3051eb925aa83c59db469d
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2019-08-31 19:59:06 -0300

    Better array input

--- OC1-DCC006-2019_2/TP1/vectorpermutation.s
+++ OC1-DCC006-2019_2/TP1/vectorpermutation.s
@@ -1,49 +1,32 @@
 .data
-  vetsize:.word 4 # Tamanho dos vetores
-  wordsize:.word 4 # Tamanho da palavra em bytes
+  array_size:.word 4 # Tamanho dos vetores
+  word_size:.word 4 # Tamanho da palavra em bytes
 
-  # Valores do primeiro vetor
-  vet00:.word 4
-  vet01:.word 3
-  vet02:.word 2
-  vet03:.word 1
+  array1:
+    .word 4
+    .word 3
+    .word 2
+    .word 1
 
-  # Valores do segundo vetor
-  vet10:.word 2
-  vet11:.word 1
-  vet12:.word 4
-  vet13:.word 3
+  array2:
+    .word 2
+    .word 1
+    .word 4
+    .word 3
 
 .text
-  lw x29, vet00
-  sw x29, 0(sp)
-  lw x29, vet01
-  sw x29, -4(sp)
-  lw x29, vet02
-  sw x29, -8(sp)
-  lw x29, vet03
-  sw x29, -12(sp)
-  lw x29, vet10
-  sw x29, -16(sp)
-  lw x29, vet11
-  sw x29, -20(sp)
-  lw x29, vet12
-  sw x29, -24(sp)
-  lw x29, vet13
-  sw x29, -28(sp)
+  lw x5, array_size # Tamanho dos vetores
+  lw x6, word_size # Tamanho da palavra em bytes
 
-  lw x5, vetsize # Tamanho dos vetores
-  lw x6, wordsize # Tamanho da palavra em bytes
+  mul x7, x5, x6 # Tamanho do vetor no stack
 
-mul x7, x5, x6 # Tamanho do vetor no stack
+  la x18, array1 # Inicio do primeiro vetor
+  add x19, x18, x7 # Fim do segundo vetor
+  sub x19, x19, x6 # Fim do segundo vetor
 
-sub x18, sp, x0 # Inicio do primeiro vetor
-sub x19, x18, x7 # Fim do segundo vetor
-add x19, x19, x6 # Fim do segundo vetor
-
-sub x20, sp, x7 # Inicio do segundo vetor
-sub x21, x20, x7 # Fim do segundo vetor
-add x21, x21, x6 # Fim do segundo vetor
+  la x20, array2
+  add x21, x20, x7 # Fim do segundo vetor
+  sub x21, x21, x6 # Fim do segundo vetor
 
 add x23, x0, x18 # Inicio do vetor a ser ordenado (primeiro)
 add x24, x0, x19 # Fim do vetor a ser ordenado (primeiro)
@@ -66,15 +49,15 @@ ecall # Termina o programa
 
 bubblesort-caller:
   add x28, x0, x0 # Marca se o bubblesort fez alguma troca
-  sub x29, x23, x0 # Endereço primeiro elemento para o primeiro teste da ordenação
-  sub x30, x23, x6 # Endereço segundo elemento para o primeiro teste da ordenação
+  add x29, x23, x0 # Endereço primeiro elemento para o primeiro teste da ordenação
+  add x30, x23, x6 # Endereço segundo elemento para o primeiro teste da ordenação
   lw x25, 0(x29) # Valor primeiro elemento para o primeiro teste da ordenação
   lw x26, 0(x30) # Valor segundo elemento para o primeiro teste da ordenação
 
   bubblesort-loop:
     beq x24, x29, bubblesort-end-loop # Se chegou no final, sai do loop
     blt x26, x25 bubblesort-invert # Se os elementos estiverem trocados, troque-os
-    
+
     beq x0, x0, bubblesort-step # Dê um passo na iteração
 
     bubblesort-invert:
@@ -87,8 +70,8 @@ bubblesort-caller:
       beq x0, x0, bubblesort-step # Dê um passo na iteração
 
     bubblesort-step:
-      sub x29, x29, x6 # Avança uma posição no vetor
-      sub x30, x30, x6 # Avança uma posição no vetor
+      add x29, x29, x6 # Avança uma posição no vetor
+      add x30, x30, x6 # Avança uma posição no vetor
       lw x25, 0(x29) # Lê os novos valores
       lw x26, 0(x30) # Lê os novos valores
 
@@ -109,8 +92,8 @@ bubblesort-caller:
       bne x25, x26, compare-arrays-fail # Se os elementos forem diferentes, dê erro
       beq x30, x19, compare-arrays-loop-end # Caso chegue no final do vetor, saia
 
-      sub x30, x30, x6 # Vai para a proxima posição do primeiro vetor
-      sub x31, x31, x6 # Vai para a proxima posição do segundo vetor
+      add x30, x30, x6 # Vai para a proxima posição do primeiro vetor
+      add x31, x31, x6 # Vai para a proxima posição do segundo vetor
 
       beq x0, x0, compare-arrays-loop # Continue o loop
 
